d7e5afb6c562a62775c6da2e9b95afb21a0d83eb
Kyle Ratti <kyleratti@users.noreply.github.com>
2019-12-04 21:06:47 -0500

    Update server-install-update.bat

--- server-install-update.bat
+++ server-install-update.bat
@@ -6,9 +6,12 @@ set /p packages_dir="Enter FULL path to packages directory:"
 echo Your watch directory will be something like:
 echo C:\Users\myuser\Dropbox\Events\BC Bowl\
 set /p watch_dir="Enter FULL path to watch directory: "
-setx PACKAGES_DIR="%packages_dir%"
-setx WATCH_DIR="%watch_dir%"
-setx NODE_ENV="PRODUCTION"
+set PACKAGES_DIR "%packages_dir%"
+set WATCH_DIR "%watch_dir%"
+set NODE_ENV "PRODUCTION"
+setx PACKAGES_DIR "%packages_dir%"
+setx WATCH_DIR "%watch_dir%"
+setx NODE_ENV "PRODUCTION"
 npm install
 npm run deploy
 @pause
