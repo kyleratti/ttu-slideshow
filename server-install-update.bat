c3f36489988457ba800e182cdadc6ea15df384d8
Kyle Ratti <kyleratti@users.noreply.github.com>
2019-12-04 21:41:15 -0500

    Update server-install-update.bat

--- server-install-update.bat
+++ server-install-update.bat
@@ -1,6 +1,18 @@
 @echo off
 git pull
 
+echo VAR: API_SERVER=%api_server%
+set /p update_api_server="WOULD YOU LIKE TO UPDATE THE API_SERVER VAR? (y/n) "
+if %update_api_server% == y (  
+  echo Your packages directory will be something like:
+  echo C:\Users\myuser\Apps\ttu-slideshow\packages\
+
+  set /p api_server="Enter FULL path to packages directory: "
+
+  set API_SERVER "%api_server%"
+  setx API_SERVER "%api_server%"
+)
+
 echo VAR: PACKAGES_DIR=%packages_dir%
 set /p update_packages_dir="WOULD YOU LIKE TO UPDATE THE PACKAGES_DIR VAR? (y/n) "
 if %update_packages_dir% == y (  
